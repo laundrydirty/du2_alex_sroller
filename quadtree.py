c64a6b70c48a27c4510727d9a498a4acf0e58158
Alex <a.sroller@seznam.cz>
2019-12-22 03:14:36 +0100

    dobrou noc

--- quadtree.py
+++ quadtree.py
@@ -1,93 +1,65 @@
-import json
-
-#vstup dat
-with open("input.geojson","r",encoding="utf-8") as f:
-    data=json.load(f)
-
-
-
-
-#vyfiltrovani souradnic bodu
-point_coordinates=[]
-for feat in data['features']:
-    coordinates=(feat['geometry']['coordinates'])
-    features=data['features']
-    point_coordinates.append(coordinates)
-print(point_coordinates)
-print(features)
-
+#nalezeni minim a maxim vstupnich dat
 def bounding_box(coordinates):
-    #seradit podle osy x
+    #serazeni podle osy x
     coordinates.sort(key = lambda p: p[0])
-    xminimum=coordinates[0]
-    xmaximum=coordinates[-1]
+    xminimum=coordinates[0][0]
+    xmaximum=coordinates[-1][0]
 
-    #seradit podle osy y
+    #serazeni podle osy y
     coordinates.sort(key=lambda p: p[1])
-    yminimum=coordinates[0]
-    ymaximum=coordinates[-1]
+    yminimum=coordinates[0][1]
+    ymaximum=coordinates[-1][0]
 
     return (xminimum,xmaximum,yminimum,ymaximum)
 
 
-
-def quadtree(points,xmin,xmax,ymin,ymax):
-    #stredy
+#funkce quadtree
+def kvad_strom(points,xmin,xmax,ymin,ymax,cluster_ID):
+    #nalezeni stredu podle kterych bude prostor dale delen
     xmid = (xmin + xmax) / 2
     ymid = (ymin + ymax) / 2
 
+    #stop podminka: pokud je v nekterem ctyruhelniku bodu mene nez 50, bude jim prirazeno clusterID
     if len(points)<50:
-        cluster_ID = cluster_number[0]
-        for index in points:
-            index['properties']['cluster_ID']=cluster_ID
-        cluster_temporary=cluster_counter.pop()
-        cluster_umber.append(cluster_temporary+1)
-        return(features)
-    else:
-        for feat in points:
-            point=feat(['geometry']['coordinates'])
+        cluster_ID[0] = cluster_ID[0] + 1
+        for i in points:
+            i['properties']['cluster_ID']=cluster_ID[0]
+        return(points)
+
+    #vytvoreni prazdnych seznamu pro jednotlive kvadranty
+    K1 = []
+    K2 = []
+    K3 = []
+    K4 = []
+
+    for pomocnefungujeto in points:
+    #rozhodovani do jakeho kvadrantu body spadaji a zapis do odpoviadjicicho seznamu
+
+        point=pomocnefungujeto['geometry']['coordinates']
+
         #1. kvadrant
-            if point[0] >= xmid and point[1] >=ymid:
+        if point[0] >= xmid and point[1] >= ymid:
                 K1.append(point)
         # 2. kvadrant
-            elif point[0] > xmid and point[1] <= ymid:
+        elif point[0] > xmid and point[1] <= ymid:
                 K2.append(point)
         #3. kvadrant
-            elif point[0] <= xmid and point[1] < ymid:
+        elif point[0] <= xmid and point[1] < ymid:
                 K3.append(point)
         #4. kvadrant
-            elif point[0] < xmid and point[1] >= ymid:
+        elif point[0] < xmid and point[1] >= ymid:
                 K4.append(point)
 
     #rekurze na jednotlive kvadranty
-    quadtree(K1,xmid,xmax,ymid,ymax)
-    quadtree(K2,xmin,xmid,ymid,ymax)
-    quadtree(K3,xmin,xmid,ymin,ymid)
-    quadtree(K4,xmid,xmax,ymin,ymid)
-
-
-
-b_box=bounding_box(point_coordinates)
-
-print(b_box)
-print((b_box[2][0]),(b_box[1][0]),(b_box[2][1]),(b_box[3][1]))
-K1 = []
-K2 = []
-K3 = []
-K4 = []
-quadtree(features,(b_box[0][0]),(b_box[1][0]),(b_box[2][1]),(b_box[3][1]))
+    kvad_strom(K1,xmid,xmax,ymid,ymax,cluster_ID)
+    kvad_strom(K2,xmin,xmid,ymid,ymax,cluster_ID)
+    kvad_strom(K3,xmin,xmid,ymin,ymid,cluster_ID)
+    kvad_strom(K4,xmid,xmax,ymin,ymid,cluster_ID)
 
 
 
 
 
-
-#seradit
-#najit minima a maxima
-#vytvorit bounding box
-
-#zacit delit na ctvrtiny, dokud vsude neni mene nez 50 bodu
-#zapsat cluster id
 
 
 
--- split.py
+++ split.py
@@ -1,25 +1,34 @@
+#import funkci z vytvoreneho modulu quadtree (a json)
 import json
-from quadtree import zkouska
+from quadtree import kvad_strom,bounding_box
 
-#vstup dat
+#vstup dat ve formatu geojson
 with open("input.geojson","r",encoding="utf-8") as f:
     data=json.load(f)
 
-
-
-#vyfiltrovani souradnic bodu
+#extrakce pouze souradnic bodu
 point_coordinates=[]
-for feat in data["features"]:
-    coordinates=(feat['geometry']['coordinates'])
+for feat in data['features']:
+    coordinates=feat['geometry']['coordinates']
     point_coordinates.append(coordinates)
 
-bounding_box(point_coordinates)
-with_ID=quadtree(point_coordinates,xmiddle,ymiddle,)
+#extrakce pouze features
+dostromu = data['features']
 
+#spusteni funkce bounding box
+xmin,xmax,ymin,ymax=bounding_box(point_coordinates)[0:4]
+print(xmin,xmax,ymin,ymax)
 
+#vytvoreni prazdnych seznamu
+cluster_ID=[0]
+
+#spusteni funkce kvad strom
+kvad_strom(dostromu,xmin,xmax,ymin,ymax,cluster_ID)
+
+#zachovani struktury geojson
 gj_structure={'type': 'FeatureCollection'}
-gj_structure['features']=with_ID
+gj_structure['features']=dostromu
 
-#vystup dat
+#vystup dat ve formatu geojson
 with open("output.geojson","w",encoding="utf-8") as f:
     json.dump(gj_structure,f,indent=2,ensure_ascii=False)
